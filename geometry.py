36dc4060ce50af03dbb6b1c3a8367fba6fc642a9
Vaibhav Datar <vaibhavdatar36@gmail.com>
2025-12-11 11:18:52 +0530

    add geomerty

--- geometry.py
+++ geometry.py
@@ -2,5 +2,5 @@ def calc_rect_area(len,bre):
     print("Area of rectangle:",len*bre)
 
 def calc_rect_peri(len,bre):
-    print("Perimiter of Rectangle:",2(len+bre))
+    print("Perimiter of Rectangle:",2*(len+bre))
 
