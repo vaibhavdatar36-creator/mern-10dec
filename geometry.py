6f0fb95bbbfc6783fb5b39fb9722cb5fd7c04816
Vaibhav Datar <vaibhavdatar36@gmail.com>
2025-12-11 10:41:47 +0530

    add geomerty

---
+++ geometry.py
@@ -0,0 +1,6 @@
+def calc_rect_area(a,b):
+    print("Area of rectangle:"(a*b))
+
+def calc_rect_peri(a,b):
+    print("Perimiter of circle:"(2(a+b)))
+
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import arithmetic as ar
+import geometry
+from geometry import calc_rect_peri as peri
+from geometry import calc_rect_area as area
+
 print("Hello ")
 a=int(input("Enter A:"))
 b=int(input("Enter B:"))
+
+len=int(input("Enter Length:"))
+bre=int(input("Enter Breadth:"))
+
 ar.add(a,b)
 ar.sub(a,b)
 ar.mult(a,b)
 ar.div(a,b)
+
+peri(len,bre)
+area(len,bre)
