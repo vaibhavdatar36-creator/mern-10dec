371be28c6e179cd23be781d4888023ffe4c34f3b
Vaibhav Datar <vaibhavdatar36@gmail.com>
2025-12-11 10:24:14 +0530

    Add Arithmetic Operation

---
+++ arithmetic.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    print("A+B=",a+b)
+
+def sub(a,b):
+    print("A-B=",a-b)
+
+def mult(a,b):
+    print("A*B=",a*b)
+
+
+def div(a,b):
+    print("A/B=",a/b)
---
+++ main.py
@@ -0,0 +1,8 @@
+import arithmetic as ar
+print("Hello ")
+a=int(input("Enter A:"))
+b=int(input("Enter B:"))
+ar.add(a,b)
+ar.sub(a,b)
+ar.mult(a,b)
+ar.div(a,b)
